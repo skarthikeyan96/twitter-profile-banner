74688ab56f89663eea98ec54ea215152fcc35bcc
Karthikeyan Shanmugam <karthikeyan@bangthetable.com>
2022-10-30 18:20:10 +0530

    fix: moving to twitter-api-client

--- app.ts
+++ app.ts
@@ -1,14 +1,20 @@
-import {Client} from 'twitter-api-sdk'
-import dotenv from 'dotenv'
+import dotenv from "dotenv";
+import { TwitterClient } from "twitter-api-client";
 
-dotenv.config()
+dotenv.config();
 
-const client = new Client(process.env.BEARER_TOKEN);
 
-const main = async () => {
-	const tweet = await client.tweets.findTweetById("1585523669630976002");
-	console.log(tweet.data);
+const client = new TwitterClient({
+  apiKey: process.env.CONSUMER_API_KEY,
+  apiSecret: process.env.CONSUMER_API_SECRET,
+  accessToken: process.env.ACCESS_TOKEN,
+  accessTokenSecret: process.env.ACCESS_TOKEN_SECRET,
+});
+
+async function updateBanner() {
+	// update the quote to the image
+	// upload the new image 
+	// update the banner
 }
 
-main()
-
+updateBanner();
